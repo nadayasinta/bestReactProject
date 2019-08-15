620f941799aaa91ce55c60dfe15c596261f1d745
zulyanor <zulyano@alterra.id>
2019-08-16 01:43:13 +0700

    fixing signout

--- src/components/header.js
+++ src/components/header.js
@@ -4,41 +4,48 @@ import React from "react";
 import { Link } from "react-router-dom";
 // import Category from "./Category";
 // import SearchField from "react-search-field";
+import { connect } from "unistore/react";
+import { actions } from "../store/store";
 
 function Header(props) {
-  return (
-    <header>
-      <div className="container-fluid ">
-        <div className="row shadow-sm">
-          <div className="col-md-12">
-            <ul className="menu">
-              <li>
-                <Link to="/">home</Link>
-              </li>
-              <li>
-                <Link to="/profile">profile</Link>
-              </li>
-              <li>
-                <Link to="/signin">sign in</Link>
-              </li>
-              <li>
-                <Link to="/">sign out</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div className="container-fluid shadow">
-        <div className="row">
-          <div className="col-md-12 p-0">
-            <div className="jumbo">
-              <h1 className="title">Astrology Master</h1>
+    return (
+        <header>
+            <div className="container-fluid ">
+                <div className="row shadow-sm">
+                    <div className="col-md-12">
+                        <ul className="menu">
+                            <li>
+                                <Link to="/">home</Link>
+                            </li>
+                            <li>
+                                <Link to="/profile">profile</Link>
+                            </li>
+                            <li>
+                                <Link to="/signin">sign in</Link>
+                            </li>
+                            <li>
+                                <Link to="/" onClick={() => props.logOut()}>
+                                    sign out
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </header>
-  );
+            <div className="container-fluid shadow">
+                <div className="row">
+                    <div className="col-md-12 p-0">
+                        <div className="jumbo">
+                            <h1 className="title">Astrology Master</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
 }
 
-export default Header;
+export default connect(
+    "isLogin",
+    actions
+)(Header);
