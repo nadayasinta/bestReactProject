98d7dd1b0d32f749e18f2666dbc972478321b4e6
fikriamri <famri@alterra.id>
2019-08-16 10:58:40 +0700

    try to solve responsive problem

--- src/pages/ZodiacDetails.js
+++ src/pages/ZodiacDetails.js
@@ -53,92 +53,81 @@ class ZodiacDetails extends React.Component {
         return (
             <div>
                 <Header />
-                <div className="wrapper ">
-                    {this.state.listZodiac
-                        .filter(
-                            zodiac =>
-                                zodiac.name === this.props.match.params.zodiac
-                        )
-                        .map(zodiac => {
-                            return (
-                                <div className="container m-5 zodiacdetails">
-                                    <div className="row justify-content-center text-center">
-                                        <div className="col-md-3 col-sm-12 text-right py-3">
-                                            <img
-                                                src={
-                                                    this.props.listimage[
-                                                        this.state.listimage.indexOf(
-                                                            this.props.match
-                                                                .params.zodiac
-                                                        )
-                                                    ]
-                                                }
-                                                width="100%"
-                                            />
-                                        </div>
-                                        <div className="col-md-9 col-sm-12 contain p-4 border shadow">
-                                            <div>
-                                                <h2 class="font-weight-bold">
-                                                    {zodiac.name.toUpperCase()}
-                                                </h2>
-                                                <h5>
-                                                    {zodiac.sun_dates[0] +
-                                                        " - " +
-                                                        zodiac.sun_dates[1]}
-                                                </h5>
-                                                <br />
+                {this.state.listZodiac
+                    .filter(
+                        zodiac => zodiac.name === this.props.match.params.zodiac
+                    )
+                    .map(zodiac => {
+                        return (
+                            <div className="container m-0 zodiacdetails wrapper">
+                                <div className="row justify-content-center text-center">
+                                    <div className="col-md-3 col-sm-12 py-3">
+                                        <img
+                                            src={
+                                                this.props.listimage[
+                                                    this.state.listimage.indexOf(
+                                                        this.props.match.params
+                                                            .zodiac
+                                                    )
+                                                ]
+                                            }
+                                            class="w-100"
+                                        />
+                                    </div>
+                                    <div className="col-md-9 col-sm-12 contain p-4 border shadow">
+                                        <h2 class="font-weight-bold">
+                                            {zodiac.name.toUpperCase()}
+                                        </h2>
+                                        <h5>
+                                            {zodiac.sun_dates[0] +
+                                                " - " +
+                                                zodiac.sun_dates[1]}
+                                        </h5>
+                                        <br />
+                                        <h4 class="font-weight-bold">
+                                            Mental Traits
+                                        </h4>
+                                        <h5>
+                                            {zodiac.mental_traits.map(trait => {
+                                                return trait + ". ";
+                                            })}
+                                        </h5>
+                                        <br />
+                                        <div class="row">
+                                            <div class="col-6">
                                                 <h4 class="font-weight-bold">
-                                                    Mental Traits
+                                                    Famous People
                                                 </h4>
-                                                <h5>
-                                                    {zodiac.mental_traits.map(
-                                                        trait => {
-                                                            return trait + ". ";
-                                                        }
-                                                    )}
+                                                <h5 className="famous">
+                                                    {zodiac.famous_people
+                                                        .slice(0, 5)
+                                                        .map(trait => {
+                                                            return (
+                                                                <h5>{trait}</h5>
+                                                            );
+                                                        })}
                                                 </h5>
-                                                <br />
-                                                <div class="row">
-                                                    <div class="col-6">
-                                                        <h4 class="font-weight-bold">
-                                                            Famous People
-                                                        </h4>
-                                                        <h5 className="famous">
-                                                            {zodiac.famous_people
-                                                                .slice(0, 5)
-                                                                .map(trait => {
-                                                                    return (
-                                                                        <h5>
-                                                                            {
-                                                                                trait
-                                                                            }
-                                                                        </h5>
-                                                                    );
-                                                                })}
-                                                        </h5>
-                                                    </div>
-                                                    <div class="col-6">
-                                                        <h4 class="font-weight-bold">
-                                                            Good Traits
-                                                        </h4>
-                                                        {zodiac.good_traits.map(
-                                                            trait => {
-                                                                return (
-                                                                    <h5>
-                                                                        {trait.toLowerCase()}
-                                                                    </h5>
-                                                                );
-                                                            }
-                                                        )}
-                                                    </div>
-                                                </div>
+                                            </div>
+                                            <div class="col-6">
+                                                <h4 class="font-weight-bold">
+                                                    Good Traits
+                                                </h4>
+                                                {zodiac.good_traits.map(
+                                                    trait => {
+                                                        return (
+                                                            <h5>
+                                                                {trait.toLowerCase()}
+                                                            </h5>
+                                                        );
+                                                    }
+                                                )}
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            );
-                        })}
-                </div>
+                            </div>
+                        );
+                    })}
             </div>
         );
     }
